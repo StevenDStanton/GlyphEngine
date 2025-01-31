64d66e2cac705d267010cc89beb4f196b636ba71
sstanton <steven@thesimpledev.com>
2025-01-31 18:02:12 -0500

    Updated test to remove deepequal

--- internal/player/player_test.go
+++ internal/player/player_test.go
@@ -1,16 +1,22 @@
 package player
 
 import (
-	"reflect"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
 	got := New()
-	want := &Player{}
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("Want %v got %v", want, got)
+	if got.x != 0 || got.y != 0 || got.movementCooldown != 0 {
+		t.Errorf("Expected (x: 0, y: 0, movementCooldown: 0), got (x: %d, y: %d, movementCooldown: %d)",
+			got.x, got.y, got.movementCooldown)
 	}
 
+	if len(got.walk) != 0 {
+		t.Errorf("Expected walk to be nil or empty, got %+v", got.walk)
+	}
+
+	if got.Level != nil {
+		t.Errorf("Expected Level to be nil, got %+v", got.Level)
+	}
 }
